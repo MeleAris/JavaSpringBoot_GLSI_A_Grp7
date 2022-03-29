10a3b11b2834766f6176db030740f5e19039f5f7
Aris <amelesusu@gmail.com>
2022-03-29 23:07:45 +0000

    Début de la modification du front-end

--- src/main/java/com/glsi_a/tp1/repository/ProduitRepository.java
+++ src/main/java/com/glsi_a/tp1/repository/ProduitRepository.java
@@ -2,7 +2,11 @@ package com.glsi_a.tp1.repository;
 
 import com.glsi_a.tp1.models.Produit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
 
 @Repository
 public interface ProduitRepository extends JpaRepository<Produit, Integer > {

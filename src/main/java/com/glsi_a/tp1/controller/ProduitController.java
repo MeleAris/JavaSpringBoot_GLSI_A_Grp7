7177791ffc2429d43390661c9919871d4cc406a2
Aris <amelesusu@gmail.com>
2022-04-18 15:11:41 +0000

    Confirmation avant suppression d'un produit

--- src/main/java/com/glsi_a/tp1/controller/ProduitController.java
+++ src/main/java/com/glsi_a/tp1/controller/ProduitController.java
@@ -56,10 +56,17 @@ public class ProduitController {
         return "redirect:/produit/show";
     }
 
-    @GetMapping("/delete/{id}")
-    public String deleteProduit(@PathVariable("id") int id)
+    @GetMapping("/conf/{id}")
+    public String deleteConf(@PathVariable("id") int id, Model model)
     {
-        produitService.deleteProduit(id);
+        model.addAttribute("leProduit", produitService.selectedProduit(id));
+        return "produit/deleteProduit";
+    }
+
+    @PostMapping("/delete/{id}")
+    public String deleteProduit(@ModelAttribute("leProduit") Produit produit)
+    {
+        produitService.deleteProduit(produit.getId());
         return "redirect:/produit/show";
     }
 }

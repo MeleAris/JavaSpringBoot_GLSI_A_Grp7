20edb531000bd6ec5f41c8bf396d16136ca88871
AceANALLA <enilra.honey@gmail.com>
2022-04-19 20:19:16 +0000

    Ajout de la classe Vente et début de l'implémentation du CRUD

---
+++ src/main/java/com/glsi_a/tp1/models/Vente.java
@@ -0,0 +1,27 @@
+package com.glsi_a.tp1.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Vente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private int quantite;
+    private double prix;
+    private LocalDate dateVente;
+
+    @ManyToOne
+    @JoinColumn(name = "produitId", nullable = false, insertable = false, updatable = false)
+    private Produit produit;
+    private int produitId;
+}
+
---
+++ src/main/java/com/glsi_a/tp1/service/VenteService.java
@@ -0,0 +1,55 @@
+package com.glsi_a.tp1.service;
+
+import com.glsi_a.tp1.models.Vente;
+import com.glsi_a.tp1.repository.VenteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VenteService {
+    @Autowired
+    private VenteRepository venteRepository;
+
+    //Création d'un nouveau produit
+    public void saveVente(Vente vente)
+    {
+        venteRepository.save(vente);
+    }
+
+    //Affichage de tous les produits
+    public List<Vente> showAllVente()
+    {
+        return venteRepository.findAll();
+    }
+
+    //Selection d'un seul produit
+    public Vente selectedVente(int id)
+    {
+        Optional<Vente> optional = venteRepository.findById(id);
+        Vente vente = null;
+        if (optional.isPresent())
+        {
+            vente = optional.get();
+        }
+        else {
+            throw new RuntimeException("Id introuvable");
+        }
+        return vente;
+    }
+
+    //Supprimer un produit
+    public void deleteVente(int id)
+    {
+        if (venteRepository.findById(id).isPresent())
+        {
+            venteRepository.deleteById(id);
+        }
+        else {
+            throw new RuntimeException("Impossible de supprimer cet élément");
+        }
+    }
+}
+

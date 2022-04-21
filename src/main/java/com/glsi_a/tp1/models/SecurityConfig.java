1078d70a4050967b599475d038b07a0485f1fd92
jackk17 <jagbegbo@gmail.com>
2022-04-21 22:36:11 +0000

    configuration de la sécurité

---
+++ src/main/java/com/glsi_a/tp1/models/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.glsi_a.tp1.models;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("admin").password("admin1234").roles("admin")
+                .and()
+                .withUser("jackk").password("12345").roles("user");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}

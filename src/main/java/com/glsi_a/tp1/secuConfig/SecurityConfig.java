057b9342e784c840821b60227ab7be3c987d53b0
jackk17 <jagbegbo@gmail.com>
2022-04-25 00:37:08 +0000

    Update SecurityConfig.java

--- src/main/java/com/glsi_a/tp1/secuConfig/SecurityConfig.java
+++ src/main/java/com/glsi_a/tp1/secuConfig/SecurityConfig.java
@@ -16,18 +16,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .inMemoryAuthentication()
-                .withUser("admin").password(passwordEncoder().encode("admin1234")).roles("admin")
+                .withUser("admin")
+                .password(passwordEncoder().encode("admin1234"))
+                .roles("admin")
                 .and()
-                .withUser("jackk").password(passwordEncoder().encode("12345")).roles("user")
+                .withUser("jackk")
+                .password(passwordEncoder().encode("12345"))
+                .roles("user")
                 .and()
-                .withUser("aris").password(passwordEncoder().encode("mel")).roles("user");
+                .withUser("aris")
+                .password(passwordEncoder().encode("mel"))
+                .roles("manager");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers("/Login.html").authenticated()
                 .and()
                 .httpBasic();
     }
